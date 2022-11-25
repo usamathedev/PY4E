96a73581df7f2eacc4216dbbb3397ff3fa9b8df6
Usama <usamathedev@gmail.com>
2022-11-25 15:14:25 +0500

    Completed ex 6.1

---
+++ PY4E Excersices/CH6/ex_6.1.py
@@ -0,0 +1,14 @@
+# Exercise 5: Take the following Python code that stores a string:
+
+# str = 'X-DSPAM-Confidence:0.8475'
+
+# Use find and string slicing to extract the portion of the string after the colon character and then use the float function to convert the extracted string into a floating point number.
+
+
+str = "X-DSPAM-Confidence:0.8475"
+
+colpos = str.find(":")
+
+floatNum = float(str[colpos+1:])
+
+print(floatNum)
