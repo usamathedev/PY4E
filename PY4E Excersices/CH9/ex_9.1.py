51d685e2aa5dd7e79e2b8c7238b183b93f771c7a
Usama <usamathedev@gmail.com>
2022-12-03 17:02:28 +0500

    examples from tutorial

---
+++ PY4E Excersices/CH9/ex_9.1.py
@@ -0,0 +1,53 @@
+# counts = dict()
+# names = ["scev", "csev", "phill", "scev", "csev", "phll"]
+
+
+# for name in names :
+
+#     counts[name] = counts.get(name, 0) + 1
+
+#     print(counts.get(name))
+#     # if name not in counts :
+#     #     counts[name] = 1
+#     # else : 
+#     #     counts[name] = counts[name] + 1
+
+# print(counts)
+
+# counts = dict()
+# print("Enter a line of text")
+# line = input('')
+
+# words = line.split()
+
+# print('Words: ', words)
+
+# print('Counting...')
+# for word in words :
+#     counts[word] = counts.get(word, 0) + 1
+
+
+# print('Counts', counts)
+
+
+# print("Enter a line of text!")
+# name = input('')
+# words = name.split()
+
+# counts = dict()
+
+# for word in words :
+#     counts[word] = counts.get(word, 0) + 1
+
+
+# bigcount = None
+# bigword = None
+
+# for word,count in counts.items() :
+#     if bigcount is None or count > bigcount :
+#         bigword = word
+#         bigcount = count
+
+
+# print(counts)
+# print(bigword, bigcount)
